6028a09146e4e7e4e00bca06aeb356d23a367934
Kazi Islam <kazichaska@gmail.com>
2022-01-09 21:00:23 -0600

    added both api's

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,17 +1,22 @@
 var citySearch = document.querySelector("h3");
 var cityInput = document.querySelector("#city-input");
-var submitButton = $("#search")
+var searchButton = $("#search")
 // document.querySelector("#btn");
 
+var defaultCity = "Minneapolis";
 
 $("#search").click(function() {
-    alert("Search button was clicked!");
+    console.log(cityInput.value);
+    var city = cityInput.value;
+    getApi(city);
+    //   localStorage.setItem(city, city);
+    // localStrData(city);
 });
 
-console.log(submitButton);
+console.log(document.querySelector("#city-input").value);
 
-function getApi() {
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=dhaka&limit=5&appid=f34dabc58db5ff52b58f3ded309b23a6'
+function getApi(city) {
+    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city ? city: defaultCity}&limit=5&appid=f34dabc58db5ff52b58f3ded309b23a6`
     
     fetch(requestUrl)
     .then(async function(response) {
@@ -27,39 +32,44 @@ function getApi() {
 
 getApi();
 
+var weatherArray = [];
 async function getWeatherDetail(lat, lon) {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?" + "lat=" +lat + "&" + "lon=" +lon + "&" + "appid=f34dabc58db5ff52b58f3ded309b23a6";
+    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&temperature=unit&appid=f34dabc58db5ff52b58f3ded309b23a6`;
     // const response = await fetch(url);
     fetch(requestUrl)
     .then(async function(response) {
         console.log(response);
-        var weatherArray = await response.json();
+        weatherArray.push(await response.json())
         console.log(weatherArray);
     });    
 }
 
+console.log(weatherArray.length);
+console.log((weatherArray[0].current.temp));
+console.log((weatherArray[0].current.wind_speed));
+console.log((weatherArray[0].current.humidity));
+console.log((weatherArray[0].current.uvi));
+
+var displayTodaysWeather = function() {
+    // get todays data from weatherarray and display 
+    var createLi = document.createElement('li');
+    for (var i = 0; i < weatherArray.length; i++) {
+        createLi.append((weatherArray[0].current.temp));
+    }
+}
 
 
+var localStrData = function(city) {
+    // localStorage.setItem(JSON.stringify(city, weatherArray[0]));
+}
 
-// after putting the city name and click on search
-// submitButton.addEventListener("click", function(event) {
-//     // event.preventDefault();
-//     console.log(event.target);
-// })
+var displayStoredCities = function() {
+    // for everything city in localsotrage append to DOM
+}
 
+displayStoredCities();
 
-// function to get city lon and lad 
-// var getCityLonLad = function() {
-//     // first api to get city detail for laditude and longitude
-//     console.log("this is where we will get api detail woth log and lad");
-// };
+var displayForcast = function() {
+    // get data from weatherarray and display weather for each forcast date
+}
 
-// // function to get the weather details
-// getCityLonLad();
-
-// var getWeatherDetail = function() {
-//     // second api to get all other detail weather using first api call's 
-//     console.log("this would be detatil view of 5-day weather");
-// }
-
-// getWeatherDetail();
